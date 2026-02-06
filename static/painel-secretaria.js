84753d9840a97d484ba8a3be27f3cb7a3424b77e
davichocoman <davichocoman18@gmail.com>
2026-02-06 09:49:26 -0300

    Update painel-secretaria.js

--- static/painel-secretaria.js
+++ static/painel-secretaria.js
@@ -1340,8 +1340,6 @@ window.addEventListener('appinstalled', () => {
     console.log('PWA: Aplicativo instalado com sucesso!');
 });
 
-let deferredPrompt;
-
 window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault();
     deferredPrompt = e;
