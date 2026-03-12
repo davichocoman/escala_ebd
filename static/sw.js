90fa5cb5e26415df55785d431aaded52f278ac8e
davichocoman <davichocoman18@gmail.com>
2026-03-11 21:06:44 -0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,11 +1,21 @@
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 
 self.addEventListener('install', (event) => {
-    self.skipWaiting(); // Força a ativação imediata
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+    );
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim()); // Assume o controle da página na hora
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                          .map(name => caches.delete(name))
+            );
+        })
+    );
 });
 
 const CACHE_NAME = 'ad-rodovia-v3'; // Mudei a versão para forçar atualização
